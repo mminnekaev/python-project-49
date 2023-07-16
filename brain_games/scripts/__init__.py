1f0eea86450dc1178005a794dbd7ad7a1dbcb84c
Marat Minnekaev <marat@Marats-MacBook-Pro.local>
2023-07-17 00:11:29 +0200

    Module and package described

---
+++ brain_games/scripts/__init__.py
@@ -0,0 +1,7 @@
+import brain_games
+
+def main():
+    brain_games.print_greeting()
+
+if __name__ == "__main__":
+    main()
---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+def print_greeting():
+    print("Welcome to the Brain Games!")
+
+def main():
+    print_greeting()
+
+if __name__ == "__main__":
+    main()
+
